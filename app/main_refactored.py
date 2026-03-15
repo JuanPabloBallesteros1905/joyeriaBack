5fcfa34ee101c1cd4ad96bb9938dd17a0c269c03
JuanPabloBallesteros1905 <juanpablo-1905@hotmail.com>
2026-03-14 23:25:51 -0500

    cors

--- app/main_refactored.py
+++ app/main_refactored.py
@@ -16,7 +16,10 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://admin.joyeriaitaliana.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
