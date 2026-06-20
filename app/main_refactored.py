1c17fe83a9d530402de5c6a2b9a0834342d63b99
JuanPabloBallesteros1905 <juanpablo-1905@hotmail.com>
2026-06-19 23:20:34 -0500

    nuevos cambios

--- app/main_refactored.py
+++ app/main_refactored.py
@@ -17,6 +17,8 @@ app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 origins = [
     "http://localhost:3000",
     "http://localhost:3001",
+    "http://127.0.0.1:4000"
+    "http://localhost:4000",
     "https://admin.joyeriaitaliana.com",
     "https://joyeriaitaliana.com",
     "https://www.joyeriaitaliana.com",
